a9a09f7361d150c0ce052ce8d9ada04bb847ffcc
mulbah11k <mulbahtkolleh@gmail.com>
2025-06-13 12:12:41 +0000

    update

--- morris_unlocker/morris_unlocker/settings.py
+++ morris_unlocker/morris_unlocker/settings.py
@@ -19,7 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tailwind',
-    'theme',
+    'morris_unlocker.theme',
     'accounts',
     'services',
     'orders',
