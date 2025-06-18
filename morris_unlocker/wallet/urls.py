f4a7c9777eb3983583ea9e32ccd4154391a8d95c
mulbah11k <mulbahtkolleh@gmail.com>
2025-06-18 12:27:00 +0000

    update with some html

--- morris_unlocker/wallet/urls.py
+++ morris_unlocker/wallet/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('dashboard/', views.wallet_dashboard, name='wallet_dashboard'),
+    path('statement/', views.statement, name='my_statement'),
+    path('invoices/', views.invoices, name='invoices'),
+    path('voucher/', views.voucher, name='r_voucher'),
 ]
--- morris_unlocker/wallet/views.py
+++ morris_unlocker/wallet/views.py
@@ -4,3 +4,12 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def wallet_dashboard(request):
     return render(request, 'wallet/wallet_dashboard.html')
+
+def statement(request):
+    return render(request, 'wallet/statement.html')
+
+def invoices(request):
+    return render(request, 'wallet/invoices.html')
+
+def voucher(request):
+    return render(request, 'wallet/recharge_voucher.html')
