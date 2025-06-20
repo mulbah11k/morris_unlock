26a3c4cff94c73459c13607070f1219e2a2e66a3
mulbah11k <mulbahtkolleh@gmail.com>
2025-06-20 11:14:43 +0000

    fix csrf problem

--- morris_unlocker/accounts/views.py
+++ morris_unlocker/accounts/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
 
 # Create your views here.
-
 
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from .forms import RegisterForm, LoginForm
+from django.views.decorators.csrf import csrf_exempt
 from .models import CustomUser
 from django.contrib.auth.decorators import login_required
 
--- morris_unlocker/morris_unlocker/settings.py
+++ morris_unlocker/morris_unlocker/settings.py
@@ -46,11 +46,11 @@ if DEBUG:
     INSTALLED_APPS.append('django_browser_reload')
 
 MIDDLEWARE = [
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
